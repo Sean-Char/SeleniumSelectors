a42616179620ceae01a525574d0b99bfb00e603c
Sean Charoutanh <scharoutanh@gmail.com>
2017-11-08 23:25:13 -0600

    Id Selector

--- NameSelector/EntryPoint.cs
+++ NameSelector/EntryPoint.cs
@@ -6,20 +6,23 @@ class EntryPoint
 {
     static void Main()
     {
+        string url = "http://testing.todvachev.com/selectors/id/";
+        string ID = "testImage";
+
         // create driver instance
         IWebDriver driver = new ChromeDriver();
 
         // go to web page
-        driver.Navigate().GoToUrl("http://testing.todvachev.com/selectors/name/");
+        driver.Navigate().GoToUrl(url);
 
         // select element on the page
-        IWebElement element = driver.FindElement(By.Name("myName"));
+        IWebElement element = driver.FindElement(By.Id(ID));
 
         // verify if element is on the page
         if (element.Displayed)
-            GreenMessage("Yes I can see the name element.");
+            GreenMessage("Yes I can see the id element.");
         else
-            RedMessage("Well something went wrong. Name element not found!");
+            RedMessage("Well something went wrong. Id element not found!");
 
         driver.Quit();
     }
