bf17fceb33adf9e9a8b4719ace15974f64b3d48d
Sean Charoutanh <scharoutanh@gmail.com>
2017-11-09 14:29:00 -0600

    added css path and x path selectors

---
+++ CssPath/EntryPoint.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/selectors/css-path/";
+        string cssPath = "#post-108 > div > figure > img"; // supported by most browser & works faster than xpath
+        string xPath = "//*[@id=\"post-108\"]/div/figure/img"; // limited support
+
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement cssPathElement = driver.FindElement(By.CssSelector(cssPath));
+        IWebElement xPathElement = driver.FindElement(By.XPath(xPath));
+
+        if (cssPathElement.Displayed)
+            GreenMessage("I can see the Css Path Element!");
+        else
+            RedMessage("Something went wrong. Css Path Element not found!");
+
+        if (cssPathElement.Displayed)
+            GreenMessage("I can see the X Path Element!");
+        else
+            RedMessage("Something went wrong. X Path Element not found!");
+
+        driver.Quit();
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
